936ebdb644e9e260fa6d6fe176d88cbf005f8f4b
DrumondGit <drumond.work@gmail.com>
2025-02-16 16:19:31 -0300

    Adding dependencies scrip and requirements file

---
+++ Lab01/install_dependencies.py
@@ -0,0 +1,15 @@
+import os
+import venv
+
+venv_dir = "venv"
+
+# Criar ambiente virtual, se não existir
+if not os.path.exists(venv_dir):
+    print("Criando ambiente virtual...")
+    venv.create(venv_dir, with_pip=True)
+
+# Instalar dependências dentro do ambiente virtual
+pip_exec = os.path.join(venv_dir, "Scripts" if os.name == "nt" else "bin", "pip")
+os.system(f"{pip_exec} install -r requirements.txt")
+
+print("Ambiente virtual configurado e dependências instaladas!")
