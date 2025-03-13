1f9e3cf1c6520610774a229f9f8df3030c514a40
DrumondGit <drumond.work@gmail.com>
2025-03-12 23:33:44 -0300

    Adding main for 1000 repositores for java

--- Lab02/src/main.py
+++ Lab02/src/main.py
@@ -1,1 +1,16 @@
-print('Hello world!!!')
+import pandas as pd
+
+import repositories_adapter
+
+# Run
+repositories = repositories_adapter.fetchRepositories()
+if repositories:
+    df = repositories_adapter.processData(repositories)
+    STATISTICS_CALCULATOR_VALUES = df[
+        ['Estrelas', 'Releases', 'Pull Requests Aceitos', 'Idade']]
+
+    if df is not None:
+        pd.set_option('display.max_rows', None)
+        pd.set_option('display.max_columns', None)
+
+        print(df.to_string())
