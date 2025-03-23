b0ea75ed4ac277e57d73f03eb5c2d041c74f62a5
DrumondGit <drumond.work@gmail.com>
2025-03-23 04:43:14 -0300

    1000 repositorios

--- Lab02/src/repositories_adapter.py
+++ Lab02/src/repositories_adapter.py
@@ -38,8 +38,8 @@ def fetchRepositories():
     """Faz a requisição GraphQL com paginação para obter 100 repositórios em 4 chamadas de 25."""
     allRepos = []
     cursor = None
-    totalRepos = 3  # Número total de repositórios desejado
-    batchSize = 1  # Repositórios por chamada
+    totalRepos = 1000  # Número total de repositórios desejado
+    batchSize = 20  # Repositórios por chamada
     numBatches = totalRepos // batchSize  # Total de chamadas necessárias
 
     for batch in range(numBatches):
