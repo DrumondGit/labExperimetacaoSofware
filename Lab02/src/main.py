b7ed30c6a3c37e3224754d6ba2f30d20f52ce3ec
DrumondGit <drumond.work@gmail.com>
2025-03-23 20:07:10 -0300

    teste pipie steps

--- Lab02/src/main.py
+++ Lab02/src/main.py
@@ -3,11 +3,16 @@ import pandas as pd
 import repositories_adapter
 
 def main():
-    # Argumentos para start e end
+    # Argumentos para start, end e quiet
     parser = argparse.ArgumentParser()
     parser.add_argument('--start', type=int, default=0, help='Índice de início')
     parser.add_argument('--end', type=int, default=1000, help='Índice final')
+    parser.add_argument('--quiet', action='store_true', help='Suprimir a saída no terminal')
     args = parser.parse_args()
+
+    # Condicional para suprimir a saída se o --quiet for fornecido
+    if not args.quiet:
+        print("Iniciando a coleta de dados dos repositórios...")
 
     # Passar start e end para fetchRepositories
     repositories = repositories_adapter.fetchRepositories(args.start, args.end)
@@ -19,7 +24,9 @@ def main():
             pd.set_option('display.max_rows', None)
             pd.set_option('display.max_columns', None)
 
-            print(df.to_string())
+            # Exibe a tabela no terminal se não for 'quiet'
+            if not args.quiet:
+                print(df.to_string())
 
             graphs = repositories_adapter.plotGraphs(df)
             repositories_adapter.generate_html_report(df, graphs)
