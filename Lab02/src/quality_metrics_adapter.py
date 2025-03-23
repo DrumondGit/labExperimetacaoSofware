e252fe43d00ea0eec61161e8a3ea70b3ed63c58a
DrumondGit <drumond.work@gmail.com>
2025-03-22 23:12:24 -0300

    teste

--- Lab02/src/quality_metrics_adapter.py
+++ Lab02/src/quality_metrics_adapter.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 def run_ck(repo_path, output_path, ck_dir):
     print(ck_dir)
-    ck_jar_path = "ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar"
+    ck_jar_path = "Lab02/src/ck-0.7.1-SNAPSHOT-jar-with-dependencies.jar"
 
     if not os.path.exists(output_path):
         os.makedirs(output_path)
